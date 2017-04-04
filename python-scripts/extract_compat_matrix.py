038c8cb9833fc93c15605d87363fc51b76d270d5
Rémi Bédard-Couture <remi.bc@outlook.com>
2017-04-04 16:26:39 -0400

    Extract compatibility matrix from caffemodel

--- python-scripts/extract_compat_matrix.py
+++ python-scripts/extract_compat_matrix.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Small script to extract the compatibility function (matrix) of a trained model
+to CSV file
 """
 
 caffe_root = '../caffe/'
@@ -20,13 +21,9 @@ PRETRAIN_FILE = 'TVG_CRFRNN_COCO_VOC.caffemodel'
 # if not this one, because too many parameters, the middle with an ellipsis"......" In the form of
 np.set_printoptions(threshold='nan')
 
-# deploy file
-#MODEL_FILE ='caffe_deploy.prototxt'
-# pre training Caffe model well
-#PRETRAIN_FILE ='caffe_iter_10000.caffemodel'
 
 # save the parameter file
-params_txt ='params.txt'
+params_txt ='compat.csv'
 pf = open (params_txt,'w')
 
 # let Caffe read network parameters in test mode
@@ -36,30 +33,27 @@ net = caffe.Net (MODEL_FILE, PRETRAIN_FILE, caffe.TEST)
 for param_name in net.params.keys ():
 
     if param_name == 'inference1':
-	# weight parameter
-	weight = net.params[param_name][0].data
-	# offset parameter
-	bias = net.params[param_name][1].data
 
-	#The # layer corresponds to the name "top" in the prototxt file
-	pf.write (param_name)
+	#write the column header
+	pf.write('background,aeroplane,bicycle,bird,boat,bottle,bus,car,cat,chair,cow,diningtable,dog,horse,motorbike,person,pottedplant,sheep,sofa,train,tvmonitor')
 	pf.write ('\n')
 
-        # write weight parameters
-	pf.write (param_name +'_weight:\n')
-	# weight parameter is a multidimensional array, in order to facilitate the output, to separate the array
-	weight.shape = (-1,1)
-
-	for w in weight:
-	    pf.write ('%f,' % w)
-
-	# write offset parameter
-	pf.write ('\n'+ param_name +'_bias:\n')
-	# bias is a multidimensional array, in order to facilitate the output, to separate the array
-	bias.shape = (-1,1)
-	for b in bias:
-	    pf.write ('%f,' % b)
-
-	pf.write ('\n\n')
+	#get the blob[2], where the compatibility matrix is stored
+	idx_data = net.params[param_name][2].data
+	#print '{0}'.format(idx_data)
+	for item in idx_data:
+	    for second in item:
+		for third in second:
+		    first=True
+		    for fourth in third:
+			if not first:
+			    pf.write(',%f' % fourth)
+			else:
+			    pf.write('%f' % fourth)
+			    first=False
+		    
+		    pf.write('\n')
+	    
+#	pf.write ('\n\n')
 
 pf.close
